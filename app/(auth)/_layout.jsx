e770a83590df79e9fd65568c82d3814780ef53b0
LuisG28 <luis.gus2869@gmail.com>
2024-09-10 14:45:13 -0600

    remove unnecesary imports

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,4 +1,3 @@
-import { View, Text } from 'react-native'
 import { Stack } from 'expo-router'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
@@ -7,20 +6,20 @@ const AuthLayout = () => {
   return (
     <>
       <Stack>
-          <Stack.Screen 
-            name='sign-in'
-            options={{
-              headerShown : false,
-            }}
-          />
-           <Stack.Screen 
-            name='sign-up'
-            options={{
-              headerShown : false,
-            }}
-          />
+        <Stack.Screen
+          name='sign-in'
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name='sign-up'
+          options={{
+            headerShown: false,
+          }}
+        />
       </Stack>
-      <StatusBar style='auto' backgroundColor='#161622'/>
+      <StatusBar style='auto' backgroundColor='#161622' />
     </>
   )
 }
