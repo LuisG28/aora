9c780b70b708997916b6b2beca41406afed15b3d
Luis Gustavo <luis.gus2869@gmail.com>
2024-09-11 22:45:44 -0600

    Add video card and custom hook

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,32 +1,32 @@
-import React, { useState } from 'react'
-import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
 import Tendring from '../../components/Tendring'
 import EmptyState from '../../components/EmptyState'
+import { getAllPost } from '../../lib/appwrite'
+import useAppWrite from '../../lib/useAppWrite'
+import VideoCard from '../../components/VideoCard'
 
 const Home = () => {
+  const { data : posts, refetch } = useAppWrite(getAllPost);
   const [refreshing, setRefreshing] = useState(false);
-  const dataList = [
-    { id: 1 },
-    { id: 2 },
-  ]
 
   const onRefresh = async () => {
     setRefreshing(true);
-
+    await refetch();
     setRefreshing(false);
   }
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={dataList}
-        key={(item) => item.id}
-        keyExtractor={(item) => item.id}
+        data={posts}
+        key={(item) => item.$id}
+        keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
+          <VideoCard video={item}/>
         )}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
