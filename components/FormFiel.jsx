0b8a992c5b867604b2f3344e16785ff298e70b8c
LuisG28 <luis.gus2869@gmail.com>
2024-09-13 18:05:23 -0600

    Remove unnecesary imports

--- components/FormFiel.jsx
+++ components/FormFiel.jsx
@@ -14,7 +14,6 @@ const FormFiel = ({
     return (
         <View className={`space-y-2 ${otherStyles}`}>
             <Text className="text-base text-gray-100 font-pmedium">{tittle}</Text>
-
             <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl 
                 focus:border-secondary items-center flex-row"
             >
