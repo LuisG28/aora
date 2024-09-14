5c8677a5a47ce3a430ad014894e108302036d30a
LuisG28 <luis.gus2869@gmail.com>
2024-09-13 20:32:58 -0600

    Init screen to upload video

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,11 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import FormField from '../../components/FormFiel'
 
 const Create = () => {
+  const [form, setForm] = useState({
+    name: '',
+    video: null,
+    thumbnail: null,
+    prompt: null, 
+  });
   return (
-    <View>
-      <Text>Create</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView className="my-6 px-4">
+        <Text className="text-2xl text-white font-psemibold">
+          Upload video
+        </Text>
+        <FormField 
+          tittle={'Video Title'}
+          value={form.name}
+          handleChangeText={(e)=>setForm({...form, name : e})}
+        />
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
